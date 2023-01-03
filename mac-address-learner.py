4c509ddda8aaa70f2bd8dd1a41512bbfd360d4f4
Cédric Metzger <metzger.cedric95@gmail.com>
2023-01-03 10:02:37 +0100

    dataframe started to configure

--- mac-address-learner.py
+++ mac-address-learner.py
@@ -1,7 +1,14 @@
 import argparse
 import json
-import sys
 import logging as log
+import sys
+import genie
+import pyats
+import re
+import netmiko
+import numpy as np
+import pandas as pd
+from netmiko import ConnectHandler
 
 
 # -------------------------------------------------------------------
@@ -17,26 +24,65 @@ import logging as log
 # -------------------------------------------------------------------
 
 # -------------------------------------------------------------------
+# FUNCITONS
+# -------------------------------------------------------------------
+
+
+def learnmacaddress(device):
+    sshex = False
+    df2 = pd.DataFrame(columns=["mac", "interface"])
+
+    try:
+        with ConnectHandler(ip=device["host"],
+                            username=device["user"],
+                            password=device["password"],
+                            device_type=device["device_type"]) as connection:
+            command = "show mac address-table"
+            mactable = connection.send_command(command, use_genie=True)
+
+    except Exception as e:
+        sshex = True
+        log.error("failed for " + device["name"] + " with exception ")
+        log.error(e)
+
+    if not sshex:
+        for vlan in mactable["mac_table"]["vlans"]:
+            for mac in mactable["mac_table"]["vlans"][vlan]["mac_addresses"]:
+                for interface in mactable["mac_table"]["vlans"][vlan]["mac_addresses"][mac]["interfaces"]:
+                    df3 = pd.DataFrame(columns=["mac", "interface"])
+                    df3 = pd.DataFrame([[mac, interface]], columns=["mac", "interface"])
+                    df3 = pd.DataFrame([["0100.0ccc.cccc", "CPU"]], columns=["mac", "interface"])
+                    # check if mac is already in DF
+                    if df3.isin(df2).any():
+                        print("found df3 in df2")
+
+                    df2.loc[len(df2)] = [mac, interface]
+
+
+    print("completed df")
+    print(df2)
+    return 0
+
+
+def updatedatabase(db):
+    return 0
+
+
+# -------------------------------------------------------------------
 # Main
 # -------------------------------------------------------------------
 
-#################################
-###Begin Main
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="""Python script to change passwords
-    Example: changePassword -f hostname.txt -vcd
+    parser = argparse.ArgumentParser(description="""Python script to learn mac addresses of connected ports
+    Example: mac-address-learner.py -lv
 """)
-    parser.add_argument('-f', '--file', help='File containing hierarchy', default='switches.json')
-    parser.add_argument('-g', '--jsonFilePath', action='store_const', const=True, help='json output file',
-                        default="json.json")
+    parser.add_argument('-f', '--file', help='file Containing switches', default='switches.json')
+    parser.add_argument('-d', '--database', help='database File', default='db.json')
+    parser.add_argument('-v', '--verbose', action='store_const', const=True, help='active verbosed logs')
     parser.add_argument('-l', '--learn', action='store_const', const=True, help='learn ports')
-    parser.add_argument('-k', '--csv', action='store_const', const=True, help='the hierarchy is csv')
-    parser.add_argument('-v', '--verbose', action='store_const', const=True, help='verbose mode')
-    parser.add_argument('-c', '--create', action='store_const', const=True, help='create hierarchy')
-    parser.add_argument('-t', '--test', action='store_const', const=True, help='test')
 
     args = parser.parse_args()
-
     # mandatory arguments
     if not args.file:
         log.error("Exiting. Filename is mandatory.")
@@ -50,6 +96,10 @@ if __name__ == "__main__":
             data = json.load(f)
 
         for switch in data['switches']:
-            log.info("learning started for " + switch['name'])
+            log.info("learning started for " + switch['host'])
+            learnmacaddress(switch)
+            # connect to switch
+            # learn mac address table
+            # Update db.json
 
     #################################
